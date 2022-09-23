6a9e6731554f1a7f9d5364f90066762ee7e4cbf0
Yoannmr2005 <yoann.mr@eduge.ch>
2022-09-23 08:42:19 +0200

    ajout de PDO et des fonctions pour l'utiliser

---
+++ www/pdo.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Site de voiture
+ * Projet du module M426
+ */
+
+require_once "/php/config.php";
+
+// Constante
+define("DB_HOST","localhost");
+define("DB_PORT", 3306);
+define("DB_NAME","");
+define("DB_USER","");
+define("DB_PASS","");
+
+/**
+ * fonction qui connecte à une DB en utilisant PDO
+ *
+ * @return PDO un objet PDO
+ */
+function db()
+{
+    static $db = null;
+    if ($db === null) {
+        $dbString = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8";
+        $db = new PDO($dbString, DB_USER, DB_PASS);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
+    return $db;
+}
+
+function dbRun($sql, $param = null){
+    $statement = db()->prepare($sql);
+    $statement->execute($param);
+    return $statement;
+}
+
