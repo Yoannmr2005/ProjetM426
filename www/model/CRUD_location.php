60d9512202e70e61a30709c810eda704caa920d8
cleaChps <clea.chps@eduge.ch>
2022-09-23 11:06:22 +0200

    CRUD location

--- www/model/CRUD_location.php
+++ www/model/CRUD_location.php
@@ -13,7 +13,7 @@
 
 function add_location($idVehicule, $prix, $dateDebut, $dateFin){
     if($prix <= 0){ 
-        echo "Le prix est trop bas.";
+        echo "Le prix est inexistant.";
         return;
     }
 
@@ -22,12 +22,37 @@ function add_location($idVehicule, $prix, $dateDebut, $dateFin){
         return;
     }
 
-    if($dateFin < $dateDebut) {
-        echo "Les dates sont erronées";
-        return;
-    }
     $sql = "INSERT INTO `vehicule`(`idVehicule`, `prix`, `dateDebut`, `dateFin`) VALUES (?, ?, ?, ?)";
     $req = dbRun($sql, [$idVehicule, $prix, $dateDebut, $dateFin]);
 }
 
-?>
+/**
+ * Fonction qui permet la modification de la location du véhicule
+ *
+ * @param int $idVehicule
+ * @param int $prix
+ * @param date $dateDebut
+ * @param date $dateFin
+ * @return void
+ */
+
+function update_Location($idVehicule, $prix, $dateDebut, $dateFin){
+    if($prix <= 0){
+        echo "Le prix est inexistant.";
+        return;
+    }
+    
+    if($dateFin < $dateDebut) {
+        echo "Les dates sont erronées";
+        return;
+    }
+
+    $req=dbRun()->prepare("UPDATE prix= :$prix, dateDebut= :$dateDebut, dateFin= :$dateFin WHERE idVehicule = :$idVehicule");
+    $req->execute(array(
+        'prix' => $nvprix,
+        'DateDebut' => $DateDebut,
+        'DateFin' => $DateFin
+    ));
+}
+
+
